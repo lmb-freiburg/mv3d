f41663a720ca4990b0443733d00d615d74a1dda1
Maxim Tatarchenko <m.tatarchenko@yahoo.com>
2016-12-19 18:01:31 +0100

    default folder creation

--- download_data.py
+++ download_data.py
@@ -8,6 +8,23 @@ snapshots_url = "http://lmb.informatik.uni-freiburg.de/\
 resources/binaries/eccv_2016_cars/snapshots.zip"
 
 
+def create_default_folders():
+    os.mkdir("logs")
+    os.mkdir("logs/nobg_nodm")
+    os.mkdir("logs/nobg_dm")
+    os.mkdir("logs/bg_nodm")
+    os.mkdir("samples")
+    os.mkdir("samples/nobg_nodm")
+    os.mkdir("samples/nobg_dm")
+    os.mkdir("samples/bg_nodm")
+    os.mkdir("samples/nobg_nodm/test")
+    os.mkdir("samples/nobg_nodm/train")
+    os.mkdir("samples/bg_nodm/test")
+    os.mkdir("samples/bg_nodm/train")
+    os.mkdir("samples/nobg_dm/test")
+    os.mkdir("samples/nobg_dm/train")
+
+
 def download_and_unpack(url, dest):
     if not os.path.exists(dest):
         try:
@@ -32,3 +49,4 @@ def download_and_unpack(url, dest):
 
 download_and_unpack(snapshots_url, "snapshots")
 download_and_unpack(data_url, "data")
+create_default_folders()
